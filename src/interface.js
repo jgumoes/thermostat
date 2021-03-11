5b36f081f67d503e54ff8a33bc91cc543846c23a
Jack Gumoes <j.gumoes@gmail.com>
2021-03-11 14:42:39 +0000

    didn't commit anything before lol. about to make the page stylish

---
+++ src/interface.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+  var thermostat = new Thermostat();
+  updateTemperature();
+  displayWeather("london");
+
+  $("#temperature-up").click(function(){
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $("#temperature-down").click(function(){
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $("#temperature-reset").click(function(){
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $("#powersaving-on").click(function(){
+    thermostat.switchPowerSavingModeOn();
+    updateTemperature();
+    $("#power-saving-status").text("on");
+  });
+
+  $("#powersaving-off").click(function(){
+    thermostat.switchPowerSavingModeOff();
+    updateTemperature();
+    $("#power-saving-status").text("off");
+  });
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+  $("#current-city").change(function() {
+    var city = $("#current-city").val();
+    displayWeather(city);
+  });
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  }
+})
---
+++ src/thermostat.js
@@ -0,0 +1,72 @@
+'use strict';
+
+class Thermostat{
+  constructor() {
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MINIMUM_TEMPERATURE = 10;
+    this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
+  }
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+
+  up() {
+    if (this._isMaximumTemperature()) {
+      return;
+    }
+    this.temperature += 1;
+  }
+
+  down() {
+    if (this._isMinimumTemperature()) {
+      return;
+    }
+    this.temperature -= 1
+  }
+
+  isPowerSavingModeOn(){
+    return this.powerSavingMode === true;
+  }
+
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+    if (this.temperature > this.MAX_LIMIT_PSM_ON){
+      this.temperature = this.MAX_LIMIT_PSM_ON
+    };
+  }
+
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+
+  energyUsage() {
+    if (this.temperature > this.HIGH_ENERGY_USAGE_LIMIT){
+      return 'high-usage';
+    }
+    if (this.temperature > this.MEDIUM_ENERGY_USAGE_LIMIT){
+      return 'medium-usage';
+    }
+    return 'low-usage';
+  }
+
+  _isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  _isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature >= this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature >= this.MAX_LIMIT_PSM_ON;
+  }
+
+};
